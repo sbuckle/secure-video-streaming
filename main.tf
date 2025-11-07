3e9d09630ebd16b4eddda3c844e2e368a5f71903
Simon Buckle <simon.buckle@gmail.com>
2025-11-07 16:26:21 +0000

    Added API gateway endpoint to the sign url function

--- main.tf
+++ main.tf
@@ -149,3 +149,36 @@ resource "aws_lambda_function" "sign_function" {
     }
   }
 }
+
+resource "aws_apigatewayv2_api" "sign_api" {
+  name          = "sign-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.sign_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.sign_function.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "get_route" {
+  api_id    = aws_apigatewayv2_api.sign_api.id
+  route_key = "GET /{proxy+}"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.sign_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.sign_function.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.sign_api.execution_arn}/*/*"
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "cloudfront_domain_name" {
   description = "The URL of the CloudFront distribution"
   value       = aws_cloudfront_distribution.cdn.domain_name
 }
+
+output "api_endpoint" {
+  value = aws_apigatewayv2_api.sign_api.api_endpoint
+}
